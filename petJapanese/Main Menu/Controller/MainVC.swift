7d68a35b6c3a71a3ceabe33a9c3986a95385d38f
Ananev Nikita <ananev1994@yandex.ru>
2020-11-08 23:40:52 +0300

    ui main vc

--- petJapanese/Main Menu/Controller/MainVC.swift
+++ petJapanese/Main Menu/Controller/MainVC.swift
@@ -16,6 +16,7 @@ class MainVC: UIViewController {
     
     @IBOutlet weak var countOfHeartLabel: UILabel!
     @IBOutlet weak var levelLabel: UILabel!
+    @IBOutlet weak var appLabel: UILabel!
     
     @IBOutlet weak var trainButton: UIButton!
     @IBOutlet weak var fightButton: UIButton!
@@ -60,16 +61,22 @@ class MainVC: UIViewController {
                         self.trainButton.setTitle("", for: .normal)
                         self.fightButton.isHidden = true
                         self.backViewForButton1.isHidden = true
+                        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.35, execute: {
+                            self.appLabel.textColor = .white
+                        })
+                        
 
 
                        }, completion: {_ in
                         UIView.animate(withDuration: 0.4) {
                             self.performSegue(withIdentifier: "toLessionList", sender: self)
-                            self.trainButton.transform = CGAffineTransform.identity
                             DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 1.0, execute: {
+                                self.trainButton.transform = CGAffineTransform.identity
                                 self.trainButton.setTitle("Train", for: .normal)
                                 self.fightButton.isHidden = false
                                 self.backViewForButton1.isHidden = false
+                                self.appLabel.textColor = #colorLiteral(red: 0.7459256053, green: 0.2329387963, blue: 0.21997118, alpha: 1)
+
                             })
                            
 
